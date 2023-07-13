cd063021bde5c47adddfbf6467ff7fc0d7c4228c
Santiago C. Saavedra <santisaavcon+commits@gmail.com>
2023-07-12 22:42:44 -0400

    refactor: bring back drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,156 @@
-import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes, Link} from "react-router-dom";
 import Home from "./home";
 import TeamRegistrationForm from "./forms/team_registration";
-import * as React from "react";
 import ErrorPage from "./errors/error_page";
 
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import {useState} from "react";
+
+
+const drawerWidth = 240;
+
+const Main = ({ open }) => {
+    return (
+        <main
+            style={{
+                flexGrow: 1,
+                padding: '20px',
+                transition: 'margin 0.2s ease-out',
+                marginLeft: open ? drawerWidth : 0,
+            }}
+        >
+            <div style={{ marginTop: '64px' }}>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/team-form" element={<TeamRegistrationForm />} />
+                    <Route path="*" element={<ErrorPage />} />
+                </Routes>
+            </div>
+        </main>
+    );
+};
+
+interface AppBarProps extends MuiAppBarProps {
+    open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+    transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: `${drawerWidth}px`,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+}));
+
 function App() {
+    const theme = useTheme();
+    const [open, setOpen] = useState(true);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
   return (
       <BrowserRouter>
-          <header>
-              <Link to={"/"}>Home </Link>
-              <Link to={"/team-form"}> Team registration</Link>
-          </header>
+          <Box sx={{ display: 'flex' }}>
+              <CssBaseline />
+              <AppBar position="fixed" open={open}>
+                  <Toolbar>
+                      <IconButton
+                          color="inherit"
+                          aria-label="open drawer"
+                          onClick={handleDrawerOpen}
+                          edge="start"
+                          sx={{ mr: 2, ...(open && { display: 'none' }) }}
+                      >
+                          <MenuIcon />
+                      </IconButton>
+                      <Typography variant="h6" noWrap component="div">
+                          Home
+                      </Typography>
+                  </Toolbar>
+              </AppBar>
+              <Drawer
+                  sx={{
+                      width: drawerWidth,
+                      flexShrink: 0,
+                      '& .MuiDrawer-paper': {
+                          width: drawerWidth,
+                          boxSizing: 'border-box',
+                      },
+                  }}
+                  variant="persistent"
+                  anchor="left"
+                  open={open}
+              >
+                  <DrawerHeader>
+                      <IconButton onClick={handleDrawerClose}>
+                          {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                      </IconButton>
+                  </DrawerHeader>
+                  <Divider />
+                  <List>
+                      <ListItem key={"register"} disablePadding>
+                          <ListItemButton component={Link} to={"/team-form"}>
+                              <ListItemIcon>
+                                  <InboxIcon />
+                              </ListItemIcon>
+                              <ListItemText primary={"Register"} />
+                          </ListItemButton>
+                      </ListItem>
 
-          <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/team-form" element={<TeamRegistrationForm />} />
-              <Route path="/error" element={<ErrorPage />} />
-          </Routes>
+                      <ListItem key={"home"} disablePadding>
+                          <ListItemButton component={Link} to={"/"}>
+                              <ListItemIcon>
+                                  <InboxIcon />
+                              </ListItemIcon>
+                              <ListItemText primary={"Home"} />
+                          </ListItemButton>
+                      </ListItem>
+                  </List>
+              </Drawer>
+              <Main open={open} />
+          </Box>
       </BrowserRouter>
   );
 }
--- src/home.tsx
+++ src/home.tsx
@@ -4,7 +4,6 @@ import {
     Box,
     Typography
 } from '@mui/material';
-import {Link} from "react-router-dom";
 
 export default function Home() {
     return (
